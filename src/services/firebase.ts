03b16b4c9c690954901997c57d2556b19778ed14
gustavo <gustavotgw@gmail.com>
2026-06-26 03:08:24 -0300

    final taskz

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,12 +3,9 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPbnOC7zQh7sU6lslEnMGP895l2saxkLo",
-  authDomain: "taskz-1afc5.firebaseapp.com",
-  projectId: "taskz-1afc5",
-  storageBucket: "taskz-1afc5.firebasestorage.app",
-  messagingSenderId: "262607624579",
-  appId: "1:262607624579:web:34acc01b8f0530adbb3eb6"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
